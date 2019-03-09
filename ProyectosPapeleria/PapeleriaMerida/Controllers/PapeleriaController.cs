0a3d67358e396d4c143af5099f16c7f2bde3406a
juicab <32874265+juicab@users.noreply.github.com>
2019-03-09 11:14:06 -0600

    PaperMIDAdmin

---
+++ ProyectosPapeleria/PapeleriaMerida/Controllers/PapeleriaController.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace PapeleriaMerida.Controllers
+{
+    public class PapeleriaController : Controller
+    {
+        // GET: Papeleria
+        public ActionResult Inicio()
+        {
+            return View();
+        }
+        public ActionResult NuestrasMarcas()
+        {
+            return View();
+        }
+        public ActionResult QuienesSomos()
+        {
+            return View();
+        }
+        public ActionResult Contacto()
+        {
+            return View();
+        }
+
+
+    }
+}
---
+++ ProyectosPapeleria/PapeleriaMerida/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="PapeleriaMerida.MvcApplication" Language="C#" %>
