436922760cab7cc83b8d6234d7ec48bde334a117
juicab <32874265+juicab@users.noreply.github.com>
2019-03-10 00:54:12 -0600

    AdminDiseño

---
+++ ProyectosPapeleria/PapeleriaMerida/Controllers/AdminController.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace PapeleriaMerida.Controllers
+{
+    public class AdminController : Controller
+    {
+        // GET: Admin
+        public ActionResult Inicio()
+        {
+            return View();
+        }
+
+        public ActionResult CatalogoPapeleria()
+        {
+            return View();
+        }
+        public ActionResult CatalogoNovedades()
+        {
+            return View();
+        }
+        public ActionResult CatalogoSticker()
+        {
+            return View();
+        }
+        public ActionResult CatalogoPapeles()
+        {
+            return View();
+        }
+        public ActionResult CatalogoBolsas()
+        {
+            return View();
+        }
+        public ActionResult Mensajes()
+        {
+            return View();
+        }
+        public ActionResult DatosEmpresa()
+        {
+            return View();
+        }
+        public ActionResult Carusel()
+        {
+            return View();
+        }
+
+
+    }
+}
