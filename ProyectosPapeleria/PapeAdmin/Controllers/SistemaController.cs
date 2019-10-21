28022e301dfb26c7d3456721f32f6d1990bbfe69
juicab <32874265+juicab@users.noreply.github.com>
2019-10-21 15:12:18 -0500

    CreacionPapeAdmin

---
+++ ProyectosPapeleria/PapeAdmin/Controllers/SistemaController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace PapeAdmin.Controllers
+{
+    public class SistemaController : Controller
+    {
+        // GET: Sistema
+        public ActionResult Inicio()
+        {
+            return View();
+        }
+    }
+}
